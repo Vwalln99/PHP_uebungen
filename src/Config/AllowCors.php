1cf2981a7515393cfcc16c139913da1033e7874f
Vwalln99 <wallner.viktoria@gmx.net>
2024-06-27 08:35:54 +0200

    fixed bugs

--- src/Config/AllowCors.php
+++ src/Config/AllowCors.php
@@ -1,18 +1,22 @@
 <?php
+
 namespace Cm\Api\Config;
 
-class AllowCors {
-		private const string ALLOW_CORS_ORIGIN_KEY = 'Access-Control-Allow-Origin';
-		private const string ALLOW_CORS_ORIGIN_VALUE = '*';
-		private const string ALLOW_CORS_METHOD_KEY = 'Access-Control-Allow-Methods';
-		private const string ALLOW_CORS_METHOD_VALUE = 'GET, POST, PUT, DELETE, PATCH, OPTIONS';
+class AllowCors
+{
+	private const ALLOW_CORS_ORIGIN_KEY = 'Access-Control-Allow-Origin';
+	private const ALLOW_CORS_ORIGIN_VALUE = '*';
+	private const ALLOW_CORS_METHOD_KEY = 'Access-Control-Allow-Methods';
+	private const ALLOW_CORS_METHOD_VALUE = 'GET, POST, PUT, DELETE, PATCH, OPTIONS';
 
-		public function init(): void {
-			$this->set(self::ALLOW_CORS_ORIGIN_KEY, self::ALLOW_CORS_ORIGIN_VALUE);
-			$this->set(self::ALLOW_CORS_METHOD_KEY, self::ALLOW_CORS_METHOD_VALUE);
-		}
+	public function init(): void
+	{
+		$this->set(self::ALLOW_CORS_ORIGIN_KEY, self::ALLOW_CORS_ORIGIN_VALUE);
+		$this->set(self::ALLOW_CORS_METHOD_KEY, self::ALLOW_CORS_METHOD_VALUE);
+	}
 
-		public function set(string $key, string $value): void {
-			header($key . ':' . $value);
-		}
+	public function set(string $key, string $value): void
+	{
+		header($key . ':' . $value);
+	}
 }
